fe6746404cd2ad4b5117c3995559028a19b564c8
Aryan Gupta <kuantum.freak@gmail.com>
2017-09-01 22:28:50 -0400

    Working str2bin Function

--- ECGR3090/test.cpp
+++ ECGR3090/test.cpp
@@ -4,25 +4,31 @@
 
 using namespace std;
 
-string db2 (string str);
+const size_t MAX_BIN = 200;
+
+void db2 (string& str);
+bitset<MAX_BIN> str2bin (std::string&& str);
 
 int main () {
 	// bitset<100> a = str2bin("123");
 	// cout << a.to_string() << endl;
 	// cout << a.to_ulong << endl;
 	
-	cout << db2("12345");
+	cout << str2bin("9999999999999999999999999999999999999999999999999999").to_string() << endl;
 }
 
-// bitset<100> str2bin (std::string&& str) {
-	// bitset<100> ret;
-	// while (str != 0) {
-	
-	// }
-	// return ret;
-// }
+bitset<MAX_BIN> str2bin (std::string&& str) {
+	bitset<MAX_BIN> ret;
+	size_t idx = 0;
+	while (str != "") {
+		ret[idx++] = (str[str.length() - 1] - '0') % 2;
+		db2(str);
+		cout << str << endl;
+	}
+	return ret;
+}
 
-string db2 (string str) {
+void db2 (string& str) {
 	int nadd = 0;
 	for (char& ch : str) {
 		int add = nadd;
@@ -32,5 +38,8 @@ string db2 (string str) {
 		
 		ch = ((ch - '0') / 2 + add) + '0';
 	}
-	return str;
+	
+	while (str[0] == '0') {
+		str.erase(str.begin());
+	}
 }
