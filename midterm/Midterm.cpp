bd5461a0d95907e47fcd28a5053bbf461ca569af
Aryan Gupta <kuantum.freak@gmail.com>
2017-10-04 08:02:27 -0400

    Add Midterm

---
+++ midterm/Midterm.cpp
@@ -0,0 +1,17 @@
+/*
+You are given an array that contains N numbers. You want to determine if there are two numbers whose sum equals a given number K. For instance, if the input is 8, 4, 1, 6, and K is 10, then the answer is true (4 and 6), whereas for K = 11, the input is false. A number may be used twice.
+
+Do the following:
+
+a. Give an O(N^2) algorithm to solve the problem.
+
+b. Give an O(N log N) algorithm to solve the problem.
+
+c. Code your solution (the better one) in the form of the function indicated below. Also, write a test program to see if your code works. You are allowed to use any library from the STL.
+*/
+bool hasNumber(vector<int>&, int K) {
+
+}
+/*
+Write the pseudo code for the 2 solutions in comments. Please upload your file as <your_lastname>.cpp
+*/
