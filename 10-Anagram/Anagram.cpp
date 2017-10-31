7c58b425dc42ec9ce05c4cc46d9dac1a620623b7
Aryan Gupta <kuantum.freak@gmail.com>
2017-10-30 21:17:53 -0400

    Revert to b0b7023 to test with updated collision algorithm

--- 10-Anagram/Anagram.cpp
+++ 10-Anagram/Anagram.cpp
@@ -31,18 +31,29 @@ int main()
 }
 
 // placement independent string hash. 
-struct pi_str_hash {
-	// Tried this without google, got a hash collision ratio of 0.827393 (which is REALLLY bad)
-	// then found this: https://stackoverflow.com/questions/18781106/
-	
-	static const std::string alpha;
-	static const std::vector<unsigned> primes;
+struct pi_str_hash { /// todo test collision resistance of this guy
+	constexpr static unsigned shift = 64;
+	constexpr static unsigned max = 0xFFFF;
 	
 	size_t operator()(const std::string& str) const {
-		uint64_t pre_hash{1};
-		for (char c : str) {
-			// std::cout << c; //  << std::endl;
-			pre_hash *= primes.at(alpha.find(c));
+	
+		uint64_t pre_hash{str.size() << 5ULL};
+		std::hash<uint64_t> alg;
+		
+		for (char ch : str) { // we want to hash the actual char and not the placement
+			uint64_t ch64 = ch * uint64_t(ch) * ch; // cast -- MAX 16777216 (25 bits)
+			
+			//uint64_t num = ch64 << ((int(ch) * ch) % shift);
+			// uint64_t num = ch64 << shift / 4;
+			// pre_hash %= num;
+			
+			// cout << num << endl;
+			
+			// for (int i = 0; i < 16; ++i) {
+				// ch64 <<= 3;
+				pre_hash += ch64;
+			// }
+			
 		}
 		
 		size_t hash = pre_hash % std::numeric_limits<size_t>::max();
@@ -50,18 +61,6 @@ struct pi_str_hash {
 		return hash;
 	}
 };
-
-const std::string pi_str_hash::alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-// I think this is 345?
-const std::vector<unsigned> pi_str_hash::primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
- 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163,
- 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271,
- 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
- 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521,
- 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647,
- 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787,
- 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929,
- 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049};
 
 struct pi_str_equal {
 	const static pi_str_hash h; // prevent it from being recreated eveytime we call this callable
