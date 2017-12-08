5710f03f1ad0d7d993cf0073502f7946b78c2128
Aryan Gupta <kuantum.freak@gmail.com>
2017-12-08 18:25:11 -0500

    Change tester file for assignment

--- Final/graph_test.cpp
+++ Final/graph_test.cpp
@@ -7,9 +7,10 @@ using namespace std;
 int main()
 {
     ifstream ifs("facebook_combined.txt");
+    //ifstream ifs("sample_edges.txt");
 	Graph fb{false, ifs};
 	
-	fb.getNewFriends(5);
+	fb.getNewFriends(0);
 	
     return 0;
 }
