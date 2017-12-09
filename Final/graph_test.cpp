46deba6e6f1d57d77d03aea486afcc18d2861a2a
Aryan Gupta <kuantum.freak@gmail.com>
2017-12-08 23:26:42 -0500

    Add timed tests

--- Final/graph_test.cpp
+++ Final/graph_test.cpp
@@ -1,8 +1,11 @@
 #include "graph.h"
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
+using clk = std::chrono::steady_clock;
 
 int main()
 {
@@ -10,7 +13,20 @@ int main()
     //ifstream ifs("sample_edges.txt");
 	Graph fb{false, ifs};
 	
+	auto start = clk::now();
+	fb.getNewFriends_2(0);
+	auto end = clk::now();
+	
+	auto time = duration_cast<milliseconds>(end - start).count();
+	
+	cout << "Time it took was " << time << " ms" << endl;
+	
+	start = clk::now();
 	fb.getNewFriends(0);
+	end = clk::now();
+	
+	time = duration_cast<milliseconds>(end - start).count();
+	cout << "Time it took was " << time << " ms" << endl;
 	
     return 0;
 }
