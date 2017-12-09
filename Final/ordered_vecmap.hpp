9bde74d62d2dd68779c0f3ffadc2d98bb7987848
Aryan Gupta <kuantum.freak@gmail.com>
2017-12-08 23:26:00 -0500

    Finish new data structure

--- Final/ordered_vecmap.hpp
+++ Final/ordered_vecmap.hpp
@@ -1,7 +1,30 @@
 
-#ifndef ORDERED_VECMAP
-#define ORDERED_VECMAP
+#ifndef ORDERED_VECMAP_HPP
+#define ORDERED_VECMAP_HPP
 
+#include <list>
+
+namespace ari {
+
+// I dont have the internet so Im going to create custom exxceptions,
+// will later go back and find the std:: versions
+
+struct key_not_found {};
+
+// I am rushing through this so this may have some bad coding practacies
+
+// This data structure gives the following benifits
+//    O(1) look up time
+//    O(1) insert time of new value
+//    O(N) insert of pre-computed value
+//    O(N) deletion
+//    Ordered data
+
+// This data structure keeys its data ordered by value_type and also offers 
+// O(1) lookup and new-insertion of a key/value pair.
+
+// This data structure assumes
+//    The default created value is the lowest value possible
 template <
 	typename TKey,
 	typename TValue //,
@@ -11,19 +34,99 @@ public:
 	using key_type   = TKey;
 	using value_type = TValue;
 	using size_type  = size_t;
+	using locator    = typename std::list<std::pair<key_type, value_type>>::iterator;
 	
-	ordered_vecmap();
-	~ordered_vecmap();
+	ordered_vecmap() : mIndex{}, mData{} {}
 	
-	void add_key(const std::pair<key_type, value_type>&)
-	void add_key()
-	void set_new_value
+	// void add_key(const std::pair<key_type, value_type>& pair) { add_key(pair.first, pair.second); }
+	// void add_key(const key_type& key, const value_type& value) {
+		// if (mIndex.find(key) == mIndex.end()) { // new key-value pair
+			// mIndex.add({key, mData.size()}); // add the key so it now points to the last member of the vector
+			// mData.emplace_back(key, 0.0); // insert new value
+		// }
+	// }
+	
+	// std::pair<key_type, value_type>& get_pair(const key_type& key) {
+		// if (mIndex.find(key) == mIndex.end())
+			// throw key_not_found{};
+		
+		// return mData[mIndex[key]];
+	// }
+	
+	// value_type get_value(const key_type& key) {
+		// if (mIndex.find(key) == mIndex.end())
+			// throw key_not_found{};
+		
+		// return mData[mIndex[key]].second;
+	// }
+	
+	// void set_new_value(const std::pair<key_type, value_type>& pair) { set_new_value(pair.first, pair.second); }
+	// void set_new_value(const key_type& key, const value_type& value) {
+		// if (mIndex.find(key) == mIndex.end())
+			// throw key_not_found{};
+		
+		// mData[mIndex[key]].second = value;
+	// }
+	
+	void add_to_value(const key_type& key, const value_type& val) {
+		// cout << "-------- Start -------------" << endl;
+		// for (auto e : mIndex) {
+			// cout << e.first << " " << &*e.second << endl;
+		// }
+		// for (auto e : mData) {
+			// cout << e.first << " " << e.second << endl;
+		// }
+		// cout << endl << endl;
+		
+		// create valuu if not exists
+		if (mIndex.find(key) == mIndex.end()) { // new key-value pair
+			mData.emplace_back(key, 0.0); // insert new value
+			mIndex.insert({key, --mData.end()}); // add the key so it now points to the last member of the vector
+		}
+		
+		// add to the value
+		locator idx = mIndex[key];
+		idx->second += val;
+		
+		// fix it so its ordered
+		// while the current value is out of ordered
+		// and we havent reached the max value
+		auto prev = std::prev(idx);
+		while (idx != mData.begin() and idx->second > prev->second) {
+			// std::iter_swap(idx, prev);// we dont want to do an
+			// auto tmp = std::move(*idx);
+			// mData.erase(idx);
+			// mData.insert(prev, tmp);
+			// idx = prev;
+			// --prev;
+			
+			mData.splice(prev, mData, idx);
+			--(--prev);
+		}
+		
+		// update our index
+		// idx contains the new index of the value, now in order
+		// mIndex[key] = idx;
+		
+		// for (auto e : mIndex) {
+			// cout << e.first << " " << &*e.second << endl;
+		// }
+		// for (auto e : mData) {
+			// cout << e.first << " " << e.second << endl;
+		// }
+		// cout << endl << endl;
+	}
+	
+	std::list<std::pair<key_type, value_type>>& get_results() { return mData; }
 	
 private:
 	
-	std::unordered_map<key_type, size_type> mIndex; // this containtains all the indexes
-	std::vector<std::pair<key_type, value_type>> mData; // this contains the actual data
+	std::unordered_map<key_type, locator> mIndex; // this containtains all the indexes
+	std::list<std::pair<key_type, value_type>> mData; // this contains the actual data
 	
 };
 
-#endif
+
+} // end namespace ari
+
+#endif // end ORDERED_VECMAP_HPP
