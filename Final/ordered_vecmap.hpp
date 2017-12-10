1fd753759c5d40e238c6fbd461d706389556af08
Aryan Gupta <kuantum.freak@gmail.com>
2017-12-09 19:03:19 -0500

    Commit before reverting to vector based vecmap

--- Final/ordered_vecmap.hpp
+++ Final/ordered_vecmap.hpp
@@ -34,7 +34,7 @@ public:
 	using key_type   = TKey;
 	using value_type = TValue;
 	using size_type  = size_t;
-	using locator    = typename std::list<std::pair<key_type, value_type>>::iterator;
+	using locator    = size_type;
 	
 	ordered_vecmap(size_type sz) : mIndex{}, mData{} {
 		mIndex.rehash(sz);
@@ -44,30 +44,32 @@ public:
 		// create valuu if not exists
 		if (mIndex.find(key) == mIndex.end()) { // new key-value pair
 			mData.emplace_back(key, 0.0); // insert new value
-			mIndex.insert({key, --mData.end()}); // add the key so it now points to the last member of the vector
+			mIndex.insert({key, mData.size()}); // add the key so it now points to the last member of the vector
 		}
 		
 		// add to the value
 		locator idx = mIndex[key];
-		idx->second += val;
+		mData[idx].second += val;
 		
 		// fix it so its ordered
 		// while the current value is out of ordered
 		// and we havent reached the max value
 		// also prevents iterator invalidation
-		auto prev = std::prev(idx);
-		while (idx != mData.begin() and idx->second > prev->second) {
-			mData.splice(prev, mData, idx);
-			--(--prev);
+		while (idx != 0 and mData[idx].second > mData[idx - 1].second) {
+			std::swap(mData[idx], mData[idx - 1]);
+			mIndex[mData[idx].first] = idx;
+			--idx;
 		}
+		
+		mIndex[key] = idx;
 	}
 	
-	std::list<std::pair<key_type, value_type>>& get_results() { return mData; }
+	std::vector<std::pair<key_type, value_type>>& get_results() { return mData; }
 	
 private:
 	
 	std::unordered_map<key_type, locator> mIndex; // this containtains all the indexes
-	std::list<std::pair<key_type, value_type>> mData; // this contains the actual data
+	std::vector<std::pair<key_type, value_type>> mData; // this contains the actual data
 	
 };
 
